8a0509e12f9b288cd280066ea4a8368f1372cde9
MuhammadZainAmmad <zainammad05@gmail.com>
2020-06-21 20:36:27 +0500

    SMIT Assignment 5 of JS

---
+++ Solutions/app.js
@@ -0,0 +1,66 @@
+//Chapter 35-38 (Functions)
+
+
+//Task#1
+
+// function currentDate() {
+//     document.write(new Date())
+// }
+// currentDate()
+
+// //Task#2
+// function greeting (fName,lName){
+//     alert('Welcome to my page '+fName+' '+lName)
+// }
+// fName=prompt('Enter your first name')
+// lName=prompt('Enter your first name')
+// greeting(fName,lName)
+
+//Task#3
+
+// function add(a,b){
+//     alert(a+b)
+// }
+// a=+prompt('Enter your first number')
+// b=+prompt('Enter your second number')
+// add(a,b)
+
+//Task#4
+
+// function cal(num1,num2,operator){
+//     if (operator==="+"){
+//         document.write(num1+num2)
+//     }
+//     else if (operator==="-"){
+//         document.write(num1-num2)
+//     }
+//     if (operator==="*"){
+//         document.write(num1*num2)
+//     }
+//     if (operator==="/"){
+//         document.write(num1/num2)
+//     }
+// }
+// num1=+prompt('Enter first number')
+// num2=+prompt('Enter second number')
+// operator=prompt("Choose an operation '+,-,*,/' ")
+// cal(num1,num2,operator)
+
+//Task#5
+
+// function square(num){
+//     alert(num**2)
+// }
+// num=+prompt('Enter number to be squared')
+// square(num)
+
+//Task#6
+
+// function factorial(num){
+//     if (num===1){
+//         return 1
+//     }
+//     return (num*factorial(num-1))   
+// }
+// num=+prompt('Enter number to get its factorial')
+// alert(factorial(num))
